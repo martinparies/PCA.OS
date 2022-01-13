499abc1bae5be4e01d0f42bcf84f0e81b986ed85
Martin Paries <martin.paries@oniris-nantes.fr>
2022-01-13 14:05:00 -0500

    test

--- R/choice.component.R
+++ R/choice.component.R
@@ -26,10 +26,10 @@ choice.component <- function(data,nature = rep("num",ncol(data)),nb.comp.to.inve
   data <- data.frame(Component = 1:nb.comp.to.investigate,Loss = round(res,4),Percentage = round(pourcentage,4) ,Improvement = round(delta,4))
   colnames(data) = c("nb.comp","Loss","Percentage (%)","Improvement  (%)")
   choice.inertia <- ggplot2::ggplot(data=data, ggplot2::aes(x= data[,1], y=data[,4])) +
-    ggplot2::geom_bar(stat="identity", width=0.5) + ggplot2::theme_classic(base_size = 15) +
+    ggplot2::geom_bar(stat="identity", width=0.5) + ggplot2::theme_classic(base_size = 10) +
     ggplot2::xlab("Number of components in the model") +
     ggplot2::ylab("Improvement of inertia") +
-    ggplot2::geom_text(ggplot2::aes(label=paste(round(delta,2),"%")), vjust= -0.1, color="black", size=5)
+    ggplot2::geom_text(ggplot2::aes(label=paste(round(delta,2),"%")), vjust= -0.1, color="black", size=3.5)
   print(choice.inertia)
   return(data)
 }
